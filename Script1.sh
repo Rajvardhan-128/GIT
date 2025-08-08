5ff63db3d54dab6279e7cffdd0c06b9846d32eee
Rajvardhan Kale <135817747+Rajvardhan-128@users.noreply.github.com>
2025-08-08 09:55:42 +0530

    Update add line 6 in  Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,3 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
+
+
+I am adding this sentence in the line 6 for file Script1.sh
