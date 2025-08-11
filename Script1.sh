00f60302173872a3711f24a12fd2d1c400823a4b
Rajvardhan Kale <135817747+Rajvardhan-128@users.noreply.github.com>
2025-08-11 08:53:58 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,5 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
 
-
-I am adding this sentence in the line 6 for file Script1.sh
+I am learning from Ygminds
